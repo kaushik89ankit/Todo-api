d655bb8b0bf8c3696e35a1abfa2ced5a5921c3bf
Ankit Kaushik <ankit@studysolo.com>
2016-07-27 00:41:00 +0530

    convert DELETE

--- server.js
+++ server.js
@@ -18,28 +18,28 @@ app.get('/', function(req, res) {
 
 app.get('/todos', function(req, res) {
 
-	var query = req.query;
-	var where ={};
-    
-    if(query.hasOwnProperty('completed')&&  query.completed==='true'){
-    	where.completed = true;
-    }else if (query.hasOwnProperty('completed') && query.completed ==='false'){
-    	where.completed = false;
+    var query = req.query;
+    var where = {};
+
+    if (query.hasOwnProperty('completed') && query.completed === 'true') {
+        where.completed = true;
+    } else if (query.hasOwnProperty('completed') && query.completed === 'false') {
+        where.completed = false;
     }
 
-    if (query.hasOwnProperty('q') && query.q.length>0) {
-    	where.description = {
+    if (query.hasOwnProperty('q') && query.q.length > 0) {
+        where.description = {
 
-    		$like: '%' + query.q + '%'
-    	};
+            $like: '%' + query.q + '%'
+        };
     }
 
-   db.todo.findAll({where:where}).then(function(todos){
-   	res.json(todos);
-   },function(e){
+    db.todo.findAll({ where: where }).then(function(todos) {
+        res.json(todos);
+    }, function(e) {
 
-   	res.status(500).send();
-   })
+        res.status(500).send();
+    })
 
 });
 
@@ -54,16 +54,16 @@ app.get('/todos', function(req, res) {
 app.get('/todos/:id', function(req, res) {
     var todoId = parseInt(req.params.id, 10);
 
-    db.todo.findById(todoId).then(function(todo){
-    	if (!!todo) {
-    	res.json(todo.toJSON());	
-    	}else{
-    		res.status(404).send();
-    	}
+    db.todo.findById(todoId).then(function(todo) {
+        if (!!todo) {
+            res.json(todo.toJSON());
+        } else {
+            res.status(404).send();
+        }
 
-    },function(e){
+    }, function(e) {
 
-    	res.status(500).send();
+        res.status(500).send();
     });
 
 });
@@ -75,11 +75,11 @@ app.post('/todos', function(req, res) {
 
     var body = _.pick(req.body, 'description', 'completed');
 
-    db.todo.create(body).then(function(todo){
-    	res.json(todo.toJSON());
-    },function(e){
+    db.todo.create(body).then(function(todo) {
+        res.json(todo.toJSON());
+    }, function(e) {
 
-    	res.status(400).json(e);
+        res.status(400).json(e);
 
     });
 
@@ -88,25 +88,28 @@ app.post('/todos', function(req, res) {
 //DELETE /todos/:id
 
 app.delete('/todos/:id', function(req, res) {
-
     var todoID = parseInt(req.params.id, 10);
-    var matchedTodo = _.findWhere(todos, {
-        id: todoID
+
+
+    db.todo.destroy({
+        where: {
+            id: todoID
+        }
+    }).then(function(rowsDeleted) {
+
+        if (rowsDeleted === 0) {
+            res.status(404).json({
+                error: 'No todo with id'
+            });
+        } else {
+            res.status(204).send();
+        }
+    }, function() {
+
+        res.status(500).send();
     });
-
-    if (!matchedTodo) {
-        res.status(404).json({
-            "error": "no to do found"
-        });
-    } else {
-
-        todos = _.without(todos, matchedTodo);
-        res.json(matchedTodo);
-
-
-    }
-
 });
+
 
 //PUT /todos/:id
 
