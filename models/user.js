ee2474019b0244d15180a82bd63693ee809c6915
Ankit Kaushik <ankit@studysolo.com>
2016-07-29 16:11:07 +0530

    Updating till S08 -E05

--- models/user.js
+++ models/user.js
@@ -41,32 +41,24 @@ module.exports = function(sequelize, DataTypes) {
         classMethods: {
             authenticate: function(body) {
                 return new Promise(function(resolve, reject) {
-                    if (typeof body.email !== 'string' || typeof body.password != 'string') {
+                    if (typeof body.email !== 'string' || typeof body.password !== 'string') {
                         return reject();
                     }
-
                     user.findOne({
                         where: {
                             email: body.email
                         }
                     }).then(function(user) {
-
                         if (!user || !bcrypt.compareSync(body.password, user.get('password_hash'))) {
                             return reject();
                         }
-
-
                         resolve(user);
 
                     }, function(e) {
                         reject();
                     });
-
-
-
                 });
             }
-
         },
         instanceMethods: {
             toPublicJSON: function() {
