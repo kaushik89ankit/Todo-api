a97109e2b071b930cab9cf92dd82f74deeb524a3
Ankit Kaushik <ankit@studysolo.com>
2016-07-30 17:30:41 +0530

    Add auth token generation to POST /users/login

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var todoNextId = 1;
 app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
-	
+
     res.send('Todo API root');
 });
 
@@ -110,59 +110,65 @@ app.put('/todos/:id', function(req, res) {
     var body = _.pick(req.body, 'description', 'completed');
     var attributes = {};
     var todoId = parseInt(req.params.id, 10);
-    
 
-    
-    if (body.hasOwnProperty('completed')){
+
+
+    if (body.hasOwnProperty('completed')) {
 
         attributes.completed = body.completed;
     }
 
-    if (body.hasOwnProperty('description')){
+    if (body.hasOwnProperty('description')) {
 
         attributes.description = body.description;
 
     }
 
-    db.todo.findById(todoId).then(function(todo){
-    	if(todo){
-    		return todo.update(attributes);
-    	}else{
-    		res.status(404).send();
-    	}
-    	},function(){
-    		res.status(500).send();
-    	}).then(function(todo){
-    		res.json(todo.toJSON());
-    	},function(e){
-    		res.status(500).send();
-    	});
+    db.todo.findById(todoId).then(function(todo) {
+        if (todo) {
+            return todo.update(attributes);
+        } else {
+            res.status(404).send();
+        }
+    }, function() {
+        res.status(500).send();
+    }).then(function(todo) {
+        res.json(todo.toJSON());
+    }, function(e) {
+        res.status(500).send();
     });
+});
 
-app.post('/users',function(req,res){
-	var body = _.pick(req.body,'email','password');
-	db.user.create(body).then(function(user){
-		res.json(user.toPublicJSON());
-	},function(e){
-		res.status(400).json(e);
-	});
+app.post('/users', function(req, res) {
+    var body = _.pick(req.body, 'email', 'password');
+    db.user.create(body).then(function(user) {
+        res.json(user.toPublicJSON());
+    }, function(e) {
+        res.status(400).json(e);
+    });
 });
 
 // POST /users/login
 
-app.post('/users/login',function(req,res){
-	var body = _.pick(req.body,'email','password');
-   	db.user.authenticate(body).then(function(user){
-   		res.json(user.toPublicJSON());
-   	},function(e){
-   		console.error(e);
-   		res.status(400).send();
-   	});
+app.post('/users/login', function(req, res) {
+    var body = _.pick(req.body, 'email', 'password');
+    console.log("The login credentials provided by users are " + body.email + ' ' + body.password);
+    db.user.authenticate(body).then(function(user) {
+
+        var token = user.generateToken('authentication');
+        //console.log("Inside authenticate function");
+        if (token) {
+            res.header('Auth',token).json(user.toPublicJSON());
+        } else {
+            res.status(401).send();
+        }
+
+    }, function() {
+        res.status(401).send();
+    });
 });
 
-
-
-db.sequelize.sync({force:true}).then(function() {
+db.sequelize.sync({ force: true }).then(function() {
     app.listen(PORT, function() {
         console.log('Express listening on ' + PORT + '!');
     });
